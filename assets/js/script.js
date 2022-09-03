1b8af7f350f045572ec39b8d7a05c65e8edb852e
Adam <github@freiik.email>
2022-09-03 03:50:19 +0200

    V2 of my personal website 🎉

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-fetch('properties.json')
-    .then(async function (response) {
-        let data = await response.json();
-        Object.keys(data).forEach(key => {
-            console.log(key, data[key])
-        })
-        document.getElementById('type').innerHTML = JSON.stringify(data, null, 2).split("\n").map(x => /"(.*)":/g.test(x) ? x.replace(/: (".*"|[+-]?([0-9]*[.])?[0-9]+)(?!:)/g, ": <span class=\"highlight\">$1</span>") : x.replace(/(".*"|[+-]?([0-9]*[.])?[0-9]+)/g, "<span class=\"highlight\">$1</span>")).map(x => x.replace(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"/, "<a href=\"$1\">$1</a>\"")).join("\n")
-    });
---
+++ assets/js/style.js
@@ -0,0 +1,16 @@
+let words = ["known as Freiik", "young developer", "high school student 🎓", "loves french bread 🥖"],
+    con = document.getElementById("underscore"),
+    letterCount = 1,
+    x = 1,
+    waiting = !1,
+    target = document.getElementById("text");
+window.setInterval(function() {
+    0 === letterCount && !1 === waiting ? (waiting = !0, target.innerHTML = words[0].substring(0, letterCount), window.setTimeout(function() {
+        var t = words.shift();
+        words.push(t), x = 1, letterCount += x, waiting = !1
+    }, 1e3)) : letterCount === words[0].length + 1 && !1 === waiting ? (waiting = !0, window.setTimeout(function() {
+        x = -1, letterCount += x, waiting = !1
+    }, 1e3)) : !1 === waiting && (target.innerHTML = words[0].substring(0, letterCount), letterCount += x)
+}, 120), window.setInterval(function() {
+    "none" === con.style.display ? con.style = "display:inline-block;" : con.style = "display:none;"
+}, 400);
