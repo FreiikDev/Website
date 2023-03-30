f4529420404a138d2a471f7b19460d1a4c4d0af7
FreiikDev <github@freiik.email>
2023-03-30 18:16:02 +0200

    color change & addition of lines in properties

--- assets/js/properties.js
+++ assets/js/properties.js
@@ -3,5 +3,5 @@ fetch('assets/js/properties.json').then(async function(response) {
     Object.keys(data).forEach(key => {
         console.log(key, data[key])
     });
-    document.getElementById('type').innerHTML = JSON.stringify(data, null, 2).split("\n").map(x => /"(.*)":/g.test(x) ? x.replace(/: (".*"|[+-]?([0-9]*[.])?[0-9]+)(?!:)/g, ": <span class=\"highlight\">$1</span>") : x.replace(/(".*"|[+-]?([0-9]*[.])?[0-9]+)/g, "<span class=\"highlight\">$1</span>")).map(x => x.replace(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"/, "<a href=\"$1\">$1</a>\"")).join("\n")
+    document.getElementById('type').innerHTML = JSON.stringify(data, null, 2).split("\n").map((x) => /"(.*)":/g.test(x) ? x.replace(/: (".*"|[+-]?([0-9]*[.])?[0-9]+)(?!:)/g, ": <span class=\"highlight\">$1</span>") : x.replace(/(".*"|[+-]?([0-9]*[.])?[0-9]+)/g, "<span class=\"highlight\">$1</span>")).map(x => x.replace(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"/, "<a href=\"$1\">$1</a>\"")).map((x,i) => `<span class="number">${i+1}.</span>  ${x}`).join("\n")
 });
